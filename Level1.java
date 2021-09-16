7b526d405bf65ccabbaed4654120d8a79f8744fc
Chr1s01 <89489807+Chr1s01@users.noreply.github.com>
2021-09-16 15:43:55 -0700

    v.1.0

--- Level1.java
+++ Level1.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class BrickWorld here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @Chris Hubbell 
+ * @version 9/16/21 v1.0
  */
 public class Level1 extends World
 {
