9f1df60e7e19f6c7e0024485544899a9edc63c72
Chr1s01 <89489807+Chr1s01@users.noreply.github.com>
2021-10-05 15:38:08 -0700

    v.1.5.2

--- Player.java
+++ Player.java
@@ -1,59 +1,65 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- */
-    public class Player extends Actor
+*/
+public class Player extends Actor
+{
+    private Health[] health;
+    private Powerup[] powerup;
+    private int speed;
+    private int walkIndex;
+    private int frame;
+    private float yVelocity;
+    private boolean isWalking;
+    private boolean isJumping;
+    private boolean isFacingLeft;
+    private final GreenfootImage[] WALK_ANIMATION;
+    private final GreenfootImage STANDING_IMAGE;
+    private final float JUMP_FORCE;
+    private final float GRAVITY;
+    private final Class NEXT_LEVEL;
+    private final GreenfootSound MUSIC;
+    int sum = 0;
+    public Player(int speed, float jumpForce, float gravity, int maxHealth,
+                  int maxPowerUp, Class nextLevel, GreenfootSound music)
     {
-        private Health[] health;
-        private Powerup[] powerup;
-        private int speed;
-        private int walkIndex;
-        private int frame;
-        private float yVelocity;
-        private boolean isWalking;
-        private boolean isJumping;
-        private boolean isFacingLeft;
-        private final GreenfootImage[] WALK_ANIMATION;
-        private final GreenfootImage STANDING_IMAGE;
-        private final float JUMP_FORCE;
-        private final float GRAVITY;
-        private final Class NEXT_LEVEL;
-        private final GreenfootSound MUSIC;
+        this.speed = speed;
+        JUMP_FORCE = jumpForce;
+        GRAVITY = gravity;
+        NEXT_LEVEL = nextLevel;
+        MUSIC = music;
         
-        public Player(int speed, float jumpForce, float gravity, int maxHealth,
-                      int maxPowerUp, Class nextLevel, GreenfootSound music)
-        {
-            this.speed = speed;
-            JUMP_FORCE = jumpForce;
-            GRAVITY = gravity;
-            NEXT_LEVEL = nextLevel;
-            MUSIC = music;
-            
-            STANDING_IMAGE = getImage();
-            WALK_ANIMATION =  new GreenfootImage[]
-                            { new GreenfootImage("walk0.png"),
-                              new GreenfootImage("walk1.png"),
-                              new GreenfootImage("walk2.png"),
-                              new GreenfootImage("walk3.png"),
-                              new GreenfootImage("walk4.png"),
-                              new GreenfootImage("walk5.png")
-                            };
-        }
-        
-        public void act()
-        {
-            walk();
-            jump();
-            fall();
-            onCollision();
-            gameOver();
-        }
-        
-        
-        public void addedToWorld(World world) {}
-        
-        private void walk()
-        {
+        STANDING_IMAGE = getImage();
+        WALK_ANIMATION =  new GreenfootImage[]
+                        { new GreenfootImage("walk0.png"),
+                          new GreenfootImage("walk1.png"),
+                          new GreenfootImage("walk2.png"),
+                          new GreenfootImage("walk3.png"),
+                          new GreenfootImage("walk4.png"),
+                          new GreenfootImage("walk5.png")
+                        };
+                         
+    for(int i = 1; i <= 9; i++)
+    {
+        sum += i;
+    }
+    System.out.println(sum);
+    }
+    
+    public void act()
+    {
+        walk();
+        jump();
+        fall();
+        onCollision();
+        gameOver();
+    }
+    
+    
+    public void addedToWorld(World world) {}
+    
+    private void walk()
+    {
         if(isWalking)
         {
             animator();
@@ -63,8 +69,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
             setImage(STANDING_IMAGE);
             walkIndex = 0;
         }
-        
-        ifGreenfoot.isKeyDown("right"))
+    
+        if(Greenfoot.isKeyDown("right"))
         {
             if(isFacingLeft)
             {
@@ -74,7 +80,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
             isFacingLeft = false;
             move(speed);
         }
-        ifGreenfoot.isKeyDown("left"))
+        
+        if(Greenfoot.isKeyDown("left"))
         {
             if(isFacingLeft)
             {
@@ -85,28 +92,65 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
             move(-speed);
         }
     }
-        private void jump() {}
-        private void fall() {}
         
+    private void jump() 
+    {
+           if(Greenfoot.isKeyDown("space") && isOnGround())
+           {
+               yVelocity = JUMP_FORCE;
+               isJumping = true;
+           }
+           
+           if(isJumping && yVelocity > 0)
+           {
+               setLocation(getX(), getY() - (int) yVelocity);
+               yVelocity -=GRAVITY;
+           }
+           else
+           {
+               isJumping = false;
+           }
+    }
+    
+    private void fall() 
+    {
+        if(!isOnGround() && !isJumping)
+        {
+            setLocation(getX(), getY() - (int) yVelocity);
+            yVelocity -= GRAVITY;
+        }
+    }
+    
     private void animator()
     {
-    if(frame % (15 - 2 * speed) == 0)
-    {
-    setImage(WALK_ANIMATION[walkIndex]);
-                walkIndex++;
-    }
-    else
-    {
-    walkIndex = 0;
-    }
-    frame++;
-        }
-        
-        private void onCollision() {}
-        private void mirrorImages() {}
-        private void gameOver() {}
-        private boolean isOnGround()
+        if(frame % (15 - 2 * speed) == 0)
         {
-            return false;
+            setImage(WALK_ANIMATION[walkIndex]);
+            walkIndex++;
         }
+        else
+        {
+            walkIndex = 0;
+        }
+        frame++;
+    }
+    
+    private void onCollision() {}
+    
+    private void mirrorImages() 
+    {
+        for(int i = 0; i < WALK_ANIMATION.length; i++)
+        {
+            WALK_ANIMATION[i].mirrorHorizontally();
+        }
+    }
+    
+    private void gameOver() {}
+    
+    private boolean isOnGround()
+    {
+        Actor ground = getOneObjectAtOffset(0, getImage().getHeight() / 2,
+                       Platform.class);
+        return ground != null;
+    }
 }
